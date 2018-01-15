d8239be3af8bf2464a15bedd09d77b6b67ea4861
Josh West <joshw@zillowgroup.com>
2018-01-15 13:11:00 -0800

    adding to html and js files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// array for business hours
+var businessHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// array for days of the week
+var daysOpen = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+// array for each location
+var storeLocation = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
