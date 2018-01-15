98a7130285c088f029fea901df4551fe0d3389b9
Josh West <joshw@zillowgroup.com>
2018-01-15 13:50:29 -0800

    working on js and html files

--- js/app.js
+++ js/app.js
@@ -7,4 +7,25 @@ var businessHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', 
 var daysOpen = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
 // array for each location
-var storeLocation = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+//var storeLocation = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+
+
+// 1st and pike
+var storeLocationOne = {
+  cookies: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
+  render: function(){ 
+
+    var ulEl = document.getElementById('pike');
+
+    for(var i = 0; i < this.cookies.length; i++){
+      // create list of items
+      var liEl = document.createElement('li');
+      // give it content
+      liEl.textContent = businessHours[i] + ': ' + this.cookies[i] + ' cookies sold.';
+      // append list to ul
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+storeLocationOne.render();
+
