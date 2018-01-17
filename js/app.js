7316b4ac472072d1789ff7fd66a77382cb84c729
Josh West <joshw@zillowgroup.com>
2018-01-17 09:34:56 -0800

    modifying files

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //global variables
 
 // array for business hours
-var businessHours = ['','6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm','total'];
+var businessHours = ['','6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm','Total'];
 
 // array to hold stores
 var allStores = [];
